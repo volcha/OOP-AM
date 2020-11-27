f3bbe2b003af1878990b10ae6474da06380b243c
volcha <57828516+volcha@users.noreply.github.com>
2020-11-28 01:55:46 +0300

    Add files via upload

---
+++ Lab1/Lab1_1/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Lab1_1 {
+    class Function {
+        public static int GetMin(int[] array) { //ищет минимальное число в массиве
+            int min = array[0];
+            for (int i = 1; i < array.Length; ++i) {
+                if (array[i] < min)
+                    min = array[i];
+            }
+            return min;
+        }
+        public int GetMax(int[] array) { //ищет максимальное число в массиве
+            int max = array[0];
+            for (int i = 1; i < array.Length; ++i) {
+                if (array[i] > max)
+                    max = array[i];
+            }
+            return max;
+        }
+        public static void factorial(int n) {
+            if (n < 0)
+                Console.WriteLine("Некоректно введённые данные");
+            else {
+                int f = 1;
+                for (int i = 2; i <= n; ++i) {
+                    f = f * i;
+                }
+                Console.WriteLine($"Факториал = {f}");
+            }
+        }
+        public void simple(int n) {
+            int fals = 0;
+            for (int i = 2; i <= n / 2; ++i) {
+                Console.WriteLine(n % i);
+                if (n % i == 0) {
+                    fals++;
+                    Console.WriteLine("Число не простое");
+                    break;
+                }
+            }
+            if (fals == 0)
+                Console.WriteLine("Число простое");
+        }
+    }
+    class Greeting {
+        string str;
+        public Greeting() {
+            str = "Hello";
+        }
+        public Greeting(string str) {
+            this.str = str;
+        }
+        public void print() {
+            Console.WriteLine(str);
+        }
+    }
+    class Program{
+        static void Main() {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] numbers = new int[n];
+            for (int i = 0; i < n; ++i) {
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int min = Function.GetMin(numbers);
+            Console.WriteLine($"Минимальное число = {min}");
+            Function max = new Function();
+            Console.WriteLine($"Максимальное число = {max.GetMax(numbers)}");
+            int fact = Convert.ToInt32(Console.ReadLine());
+            Function.factorial(fact);
+            int num = Convert.ToInt32(Console.ReadLine());
+            Function obj = new Function();
+            obj.simple(num);
+
+            Greeting g, g1, g2, g3;
+            g = new Greeting();
+            g1 = new Greeting("Good morning");
+            g2 = new Greeting("Good evening");
+            g3 = new Greeting("Good night");
+            g.print();
+            g1.print();
+            g2.print();
+            g3.print();
+        }
+    }
+}
---
+++ Lab1/Lab1_2/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+
+namespace Lab1_2 {
+    class Program {
+        class A {
+            B objB = null;
+            public B b {
+                get {
+                    Console.Write("a -> b ");
+                    return objB;
+                }
+            }
+            C objC = null;
+            public C c {
+                get {
+                    Console.Write("a -> c ");
+                    return objC;
+                }
+            }
+            J objJ = null;
+            public J j {
+                get {
+                    Console.Write("a -> j ");
+                    return objJ;
+                }
+            }
+            public A(B b, C c, J j) {
+                objB = b;
+                objC = c;
+                objJ = j;
+            }
+            public void methA() {
+                Console.WriteLine("hash={0} Method A", this.GetHashCode());
+            }
+        }
+        class B {
+            D objD = null;
+            public D d {
+                get {
+                    Console.Write("-> d ");
+                    return objD;
+                }
+            }
+            public B(D d) {
+                objD = d;
+            }
+            public void methB() {
+                Console.WriteLine("hash={0} Method B", this.GetHashCode());
+            }
+        }
+        class C {
+            E objE = null;
+            public E e {
+                get {
+                    Console.Write("-> e ");
+                    return objE;
+                }
+            }
+            F objF = null;
+            public F f {
+                get {
+                    Console.Write("-> f ");
+                    return objF;
+                }
+            }
+            K objK = null;
+            public K k {
+                get {
+                    Console.Write("-> k ");
+                    return objK;
+                }
+            }
+            public C(E e, F f, K k) {
+                objE = e;
+                objF = f;
+                objK = k;
+            }
+            public void methC() {
+                Console.WriteLine("hash={0} Method C", this.GetHashCode());
+            }
+        }
+        class J {
+            public J() { }
+            public int vJ = 0;
+            public J(int val) {
+                vJ = val;
+            }
+            public void methJ() {
+                Console.WriteLine("hash={0} Method J", this.GetHashCode());
+            }
+        }
+        class D {
+            public D() { }
+            public int vD = 7;
+            public D(int val) {
+                vD = val;
+            }
+            public void methD() {
+                Console.WriteLine("hash={0} Method D", this.GetHashCode());
+            }
+        }
+        class E {
+            public E() { }
+            public int vE = 202;
+            public E(int val) {
+                vE = val;
+            }
+            public void methE() {
+                Console.WriteLine("hash={0} Method E", this.GetHashCode());
+            }
+        }
+        class F {
+            public F() { }
+            public int vF = 67;
+            public F(int val) {
+                vF = val;
+            }
+            public void methF() {
+                Console.WriteLine("hash={0} Method F", this.GetHashCode());
+            }
+        }
+        class K {
+            public K() { }
+            public int vK = 31;
+            public K(int val) {
+                vK = val;
+            }
+            public void methK() {
+                Console.WriteLine("hash={0} Method K", this.GetHashCode());
+            }
+        }
+        static void Main() {
+            K objK = new K();
+            F objF = new F();
+            E objE = new E();
+            D objD = new D();
+            J objJ = new J();
+            C objC = new C(objE, objF, objK);
+            B objB = new B(objD);
+            A objA = new A(objB, objC, objJ);
+            objA.methA();
+            objB.methB();
+            objA.b.methB();
+            objC.methC();
+            objA.c.methC();
+            objJ.methJ();
+            objA.j.methJ();
+            objD.methD();
+            objA.b.d.methD();
+            objE.methE();
+            objA.c.e.methE();
+            objF.methF();
+            objA.c.f.methF();
+            objK.methK();
+            objA.c.k.methK();
+        }
+    }
+}
